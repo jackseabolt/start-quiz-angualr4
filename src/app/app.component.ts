e33836ed38f4dbfa2ede61c3dda7fa666dbb1e5e
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-03-08 16:48:21 -0800

    top section complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgRedux, select } from 'ng2-redux'; 
+import { AppState } from './reducers/reducer'; 
+import { togglePicture } from './actions/actions'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent  { 
+  constructor(private ngRedux: NgRedux<AppState>) {}
+  @select('photoDisplayed') photoDisplayed;
+
+  quizDisplayed: false; 
 }
---
+++ src/app/nasa-photo/nasa-photo.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core'; 
+import { AppState } from '../reducers/reducer'; 
+import { NgRedux, select } from 'ng2-redux'; 
+import { togglePicture, togglePictureInfo } from '../actions/actions'; 
+
+@Component({
+    selector: 'nasaPhoto',
+    templateUrl: './nasa-photo.component.html', 
+    styleUrls: ['./nasa-photo.component.css']
+})
+
+export class NasaPhoto {
+    constructor(private ngRedux: NgRedux<AppState>) {}
+
+    @select('nasaImageUrl') nasaImageUrl; 
+    @select('infoOn') infoOn; 
+
+    photoToggle() {
+        this.ngRedux.dispatch(togglePicture()); 
+    }
+
+    infoToggle() {
+        this.ngRedux.dispatch(togglePictureInfo()); 
+    }
+}
